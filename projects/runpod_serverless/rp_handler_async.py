6845575d37809e979f02ed31f8c9425acfb74010
sarath-menon <sarathmenon.downey@gmail.com>
2025-01-02 20:47:23 +0000

    added mock async handler

---
+++ projects/runpod_serverless/rp_handler_async.py
@@ -0,0 +1,89 @@
+import os
+import logging
+import shutil
+from pathlib import Path
+import base64
+import asyncio
+
+import runpod
+from runpod.serverless.utils import rp_download, rp_cleanup
+from runpod.serverless.utils.rp_validator import validate
+from flux_server.custom_types import TrainingParams, INPUT_SCHEMA
+import flux_server.train
+
+from utils import save_base64_images, zip_images
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
+async def run(job):
+    '''
+    RunPod async handler for training requests
+    '''
+    job_input = job['input']
+    
+    # Validate inputs
+    if 'errors' in (job_input := validate(job_input, INPUT_SCHEMA)):
+        return {'error': job_input['errors']}
+
+    job_input = job_input['validated_input']
+
+    if not job_input.get('base64_images') and not job_input.get('zip_url'):          
+        return {'error': 'Images must be provided as base64 strings or a ZIP file URL'} 
+
+    dataset_dir = Path("input_images")
+    dataset_dir.mkdir(parents=True, exist_ok=True)
+
+    # Create tasks for image processing
+    tasks = []
+    if 'zip_url' in job_input:
+        tasks.append(asyncio.create_task(zip_images(job_input['zip_url'], dataset_dir)))
+
+    if 'base64_images' in job_input:    
+        tasks.append(asyncio.create_task(save_base64_images(job_input['base64_images'], dataset_dir)))
+    
+    # Wait for all image processing tasks to complete
+    if tasks:
+        await asyncio.gather(*tasks)
+        
+    # Parse and validate training parameters
+    try:
+        params_dict = TrainingParams.parse_obj(job_input['training_params'])
+        params_dict = params_dict.dict(exclude_none=True)
+    except Exception as e:
+        logger.error(f"Failed to parse parameters: {str(e)}")
+        return {"error": f"Invalid training parameters: {str(e)}"}
+    
+    # Run training
+    try:
+        logger.info("Starting training process")
+        output_path = await flux_server.train.handle_training(
+            # input_images_path=str(temp_path),
+            **params_dict
+        )
+        
+        logger.info(f"Training completed successfully. Output at {output_path}")
+        return {
+            "status": "success",
+            "output_path": str(output_path)
+        }
+            
+    except Exception as e:
+        logger.error(f"Training failed: {str(e)}", exc_info=True)
+        return {"error": str(e)}
+    
+    finally:
+        # Cleanup
+        rp_cleanup.clean(['input_objects'])
+        # delete the input images   
+        shutil.rmtree(dataset_dir)
+
+runpod.serverless.start({
+    "handler": run,
+    "return_aggregate_stream": True
+})
