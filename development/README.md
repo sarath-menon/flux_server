ed21f57e82c38ead8166eec3b230d4b618f4ff62
Sarath S Menon <sarathmenon.downey@gmail.com>
2025-01-11 11:16:36 +0100

    added runpod worker async demo

--- development/README.md
+++ development/README.md
@@ -1,6 +1,19 @@
+# Usage
 
-Start runpod worker 
+Start runpod worker (backend server)
+
 ```
-python rp_handler_async.py --rp_serve_api --rp_api_port 8006
+python runpod_workers/rp_handler.py --rp_serve_api --rp_api_port 8006
 ```
 
+Start webhook server
+
+```
+python rp_client.py
+```
+
+Start python client
+
+```
+python webhook_server.py
+```
--- development/runpod_workers/rp_handler_async.py
+++ development/runpod_workers/rp_handler_async.py
@@ -1,15 +1,19 @@
 import runpod
 import asyncio
 
-def async_generator_handler(job):
+async def async_generator_handler(job):
     job_input = job["input"]
     the_number = job_input["number"]
 
     if not isinstance(the_number, int):
-        return {"error": "Please provide an integer."}
+        yield {"error": "Please provide an integer."}
+        return
 
     for i in range(the_number):
         yield i
+
+        # Simulate an asynchronous task, such as processing time for a large language model
+        await asyncio.sleep(1)
 
 # Configure and start the RunPod serverless function
 runpod.serverless.start(
